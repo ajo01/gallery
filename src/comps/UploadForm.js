b8e71aa7aaaa4601f326e5cdf129bf2a116fc955
ajo01 <amyjojjm2001@gmail.com>
2021-06-12 20:31:12 -0700

    useStorage hook + config import

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import ProgressBar from './ProgressBar'
 
 const UploadForm = () => {
     const [file, setFile] = useState(null)
@@ -9,7 +10,6 @@ const UploadForm = () => {
 
     const changeHandler = (e) => {
         let selected = e.target.files[0]
-        // console.log(selected)
 
         if (selected && types.includes(selected.type)) {
             setFile(selected)
@@ -22,10 +22,14 @@ const UploadForm = () => {
 
     return (
         <form>
+            <label>
             <input type="file" onChange={changeHandler}/>
+            <span>+</span>
+            </label>
             <div className="output">
                 {error && <div className="error">{error}</div>}
                 {file && <div className="fileName">{file.name}</div>}
+                {file && <ProgressBar file={file} setFile={setFile}/>}
             </div>
         </form>
     )
