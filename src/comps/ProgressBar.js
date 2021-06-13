1843bbc4389dede076cf165e4f97cb0244036c05
ajo01 <amyjojjm2001@gmail.com>
2021-06-12 20:40:18 -0700

    progressbar reverts when done

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,11 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import useStorage from '../hooks/useStorage'
 
 const ProgressBar = ({file, setFile}) => {
     const {url, progress} = useStorage(file)
-    console.log(progress, url)
+    useEffect(() => {
+        if (url) {
+            setFile(null)
+        }
+    }, [url, setFile]) 
+
     return (
-        <div className="progress-bar">progress</div>
+        <div className="progress-bar" style={{width:progress + '%'}}></div>
     )
 }
 
